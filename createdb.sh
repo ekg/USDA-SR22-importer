8e6c2998a30f28ff8b470814e7892847eb4e42b4
Adam J. Henry <adam.j.henry@gmail.com>
2014-02-27 00:29:34 -0600

    Several changes made to allow other USDA Standard Reference versions.
    - createdb.sh now defaults to version "22", but can be changed.
    - Download from script instead of Makefile.
    - Use custom schema file, if present, or default to mdb-schema.

--- createdb.sh
+++ createdb.sh
@@ -4,35 +4,49 @@
 #  (http://www.ars.usda.gov/Services/docs.htm?docid=18879)
 # draws heavily on http://blog.moybella.net/2007/03/10/converting-microsoft-access-mdb-into-csv-or-mysql-in-linux/
 
-SR22=data/sr22.mdb # data from the USDA in m$ access format
+VER=22
 MYSQLUSER=youruser
 MYSQLPASS=yourpass
 MYSQLDB=ndb
 MYSQLCONN="mysql -u $MYSQLUSER --password=$MYSQLPASS $MYSQLDB"
-TABLES=`mdb-tables $SR22` # a list of tables in our input
+MDB=data/sr${VER}.mdb # data from the USDA in m$ access format
 
-# create the database if it doesn't exist
-
-echo creating database $MYSQLDB
-echo "CREATE DATABASE IF NOT EXISTS $MYSQLDB;" | $MYSQLCONN || exit 1
+# Downloading SR database.
+if [[ -f data/sr${VER}.mdb ]]
+then
+	continue
+elif [[ -f data/sr${VER}db.zip ]]
+then
+	unzip data/sr${VER}db.zip sr${VER}.mdb
+	mv sr${VER}.mdb data/
+else
+	wget http://www.ars.usda.gov/SP2UserFiles/Place/12354500/Data/SR${VER}/dnload/sr${VER}db.zip || exit 1
+	mv sr${VER}db.zip data/
+	unzip data/sr${VER}db.zip sr${VER}.mdb
+	mv sr${VER}.mdb data/
+fi
 
 # put the schema into the database
-
 echo creating schema in $MYSQLDB
-#mdb-schema $SR22 | $MYSQLCONN
-# The above should work, but the schema requires manual tweaking.
-# The schema that mdb-schema produces from the sr22 access database does not
-# match mysql's spec.
-# Instead we use this hand-edited schema:
-$MYSQLCONN <sr22.sql
 
+# If custom schema is available, use it. If not, use what is available.
+if [[ -f sr${VER}.sql ]]
+then
+	$MYSQLCONN <sr${VER}.sql
+else	
+	mdb-schema --drop-table $MDB mysql | $MYSQLCONN
+fi
+
+TABLES=`mdb-tables $MDB` # a list of tables in our input
 echo adding tables to $MYSQLDB
+
 for table in $TABLES
 do
-    echo adding $table
-    mdb-export -H $SR22 $table > $table.sql
-        mysqlimport -L -u $MYSQLUSER --password=$MYSQLPASS --fields-terminated-by="," --fields-enclosed-by="\"" $MYSQLDB $table.sql
-        rm $table.sql
+	echo adding $table
+	mdb-export -H $MDB $table > $table.sql
+	mysqlimport -L -u $MYSQLUSER --password=$MYSQLPASS --fields-terminated-by="," --fields-enclosed-by="\"" $MYSQLDB $table.sql
+	rm $table.sql
 done
 
 echo done
+
